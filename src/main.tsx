f8f0dd0d8366aad43fea4ea07a0a1e26141552b5
Aleks <san4es.kom@gmail.com>
2024-02-25 19:00:36 +0200

    feat: Implementation of aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import WrappedApp from './App';
+import WrappedApp from '@components/Layout/App/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
