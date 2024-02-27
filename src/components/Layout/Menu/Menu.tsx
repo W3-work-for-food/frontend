57d6bae1b3b9941119bea40e1e1afd74b38c5384
Aleks <san4es.kom@gmail.com>
2024-02-27 12:46:44 +0200

    feat: Adding Menu component.

---
+++ src/components/Layout/Menu/Menu.tsx
@@ -0,0 +1,61 @@
+import { MenuItem, MenuList, styled } from '@mui/material';
+import vars from '@styles/_export.module.scss';
+import styles from './Menu.module.scss';
+
+const menuItems = [
+  {
+    id: 1,
+    name: 'Уведомления',
+  },
+  {
+    id: 2,
+    name: 'Aмбассадоры',
+  },
+  {
+    id: 3,
+    name: 'Дашборд',
+  },
+];
+
+const CustomList = styled(MenuList)(() => ({
+  '&.MuiList-root': {
+    padding: '0',
+    gap: '12px',
+    maxWidth: '144px',
+    margin: '0 auto',
+  },
+}));
+
+const CustomMenuItem = styled(MenuItem)(() => ({
+  '&.MuiMenuItem-root': {
+    padding: '8px 0 8px 24px',
+    borderRadius: '16px',
+    width: '100%',
+    '&:hover': {
+      backgroundColor: vars.hoverFiledColor,
+    },
+    '&:focus': {
+      backgroundColor: vars.globalWhite,
+    },
+  },
+}));
+
+const Menu = () => {
+  return (
+    <nav className={styles.menu}>
+      <CustomList className={styles.menu__list}>
+        {menuItems.map((item) => (
+          <CustomMenuItem
+            className={styles.menu__item}
+            key={item.id}
+            disableRipple
+          >
+            {item.name}
+          </CustomMenuItem>
+        ))}
+      </CustomList>
+    </nav>
+  );
+};
+
+export default Menu;
