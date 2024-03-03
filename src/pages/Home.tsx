a1cd23d793d4609b03f8414a6a8dbda0eaa9352a
Aleks <san4es.kom@gmail.com>
2024-03-03 15:04:54 +0200

    feat: Content area created. Some refactoring.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,18 @@
 import { notificationPageTableColumns } from '@constants/tableColumns';
 import { notificationsRows } from '@constants/tableRows';
 import Table from '@/components/Table/Table';
+import Content from '@/components/Layout/Content/Content';
 
 const Home = () => {
   return (
     <>
       <div style={{ gridArea: 'controls', height: '76px' }} />
-      <div style={{ gridArea: 'content' }}>
+      <Content>
         <Table
           columns={notificationPageTableColumns}
           rows={notificationsRows}
         />
-      </div>
+      </Content>
     </>
   );
 };
