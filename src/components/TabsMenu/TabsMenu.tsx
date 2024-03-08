99bac7147058b17ccc41f75dce905b5e905aa266
Aleks <san4es.kom@gmail.com>
2024-03-08 18:28:36 +0200

    feat: some style fixes in TabsMenu

--- src/components/TabsMenu/TabsMenu.tsx
+++ src/components/TabsMenu/TabsMenu.tsx
@@ -6,8 +6,13 @@ import { styled } from '@mui/material/styles';
 import vars from '@styles/_export.module.scss';
 import style from './TabsMenu.module.scss';
 
+export enum TabsVariants {
+  NOTIFICATIONS = 'Notifications',
+  AMBASSADOR_PAGE = 'AmbassadorPage',
+}
+
 interface TabsMenuProps {
-  variant: 'Notifications' | 'AmbassadorPage';
+  variant: TabsVariants.NOTIFICATIONS | TabsVariants.AMBASSADOR_PAGE;
 }
 
 const StyledTab = styled(Tab)(() => ({
@@ -15,14 +20,26 @@ const StyledTab = styled(Tab)(() => ({
     color: `${vars.subtitlesColor}`,
     textTransform: 'none',
     letterSpacing: '-0.1px',
-    margin: '0 12px',
-    padding: '5px 0 0 0',
+    padding: '0',
   },
   '&:focus': {
     color: `${vars.mainTextColor}`,
   },
   '&.Mui-selected': {
     color: `${vars.mainTextColor}`,
+  },
+}));
+
+const CustomTabs = styled(Tabs)(() => ({
+  '.MuiTabs-flexContainer': {
+    gap: '24px',
+  },
+  '&.MuiTabs-root': {
+    minHeight: '32px',
+  },
+  '& .MuiButtonBase-root': {
+    justifyContent: 'space-between',
+    minHeight: '32px',
   },
 }));
 
@@ -33,13 +50,13 @@ const TabsMenu: React.FC<TabsMenuProps> = ({ variant }) => {
   };
 
   const tabLabels =
-    variant === 'Notifications'
+    variant === TabsVariants.NOTIFICATIONS
       ? ['Прочитанные', 'Непрочитанные']
       : ['Общая информация', 'Размещенный контент', 'Программа лояльности'];
 
   return (
-    <Box>
-      <Tabs
+    <Box style={{ height: '32px' }}>
+      <CustomTabs
         onChange={handleChange}
         value={value}
         aria-label="Menu tabs"
@@ -55,7 +72,7 @@ const TabsMenu: React.FC<TabsMenuProps> = ({ variant }) => {
             disableRipple
           />
         ))}
-      </Tabs>
+      </CustomTabs>
     </Box>
   );
 };
