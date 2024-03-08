ad0e77ae8be08624054632015e1659a5a9acb4e6
Aleks <san4es.kom@gmail.com>
2024-03-08 19:07:37 +0200

    feat: containers for each Tab Created

---
+++ src/components/UserContent/CommonInformation/CommonInformation.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+const CommonInformation = () => {
+  return (
+    <Box style={{width: '100%', height: '100%'}} component="div">
+      Общая информация
+    </Box>
+  );
+};
+
+export default CommonInformation;
--- src/components/UserContent/UserContent.tsx
+++ src/components/UserContent/UserContent.tsx
@@ -2,8 +2,11 @@ import { Box } from '@mui/material';
 import TabsMenu, { TabsVariants } from '@components/TabsMenu/TabsMenu';
 import { IconButton } from '@components/ui/Buttons/Buttons';
 import Edit from '@icons/Edit';
-import styles from './UserContent.module.scss';
 import * as React from 'react';
+import styles from './UserContent.module.scss';
+import CommonInformation from '@components/UserContent/CommonInformation/CommonInformation';
+import LoyaltyProgram from '@components/UserContent/LoyaltyProgram/LoyaltyProgram';
+import PostedContent from '@components/UserContent/PostedContent/PostedContent';
 
 const UserContent = () => {
   const [value, setValue] = React.useState(0);
@@ -11,13 +14,27 @@ const UserContent = () => {
     setValue(newValue);
   };
 
+  let UserData;
+  if (value === 0) {
+    UserData = <CommonInformation />
+  } else if (value === 1) {
+    UserData = <PostedContent />
+  } else {
+    UserData = <LoyaltyProgram />
+  }
+
+
   return (
     <Box className={styles.userContent} component="div">
       <Box className={styles.userContent__heading} component="div">
-        <TabsMenu value={value} onChange={handleChange} variant={TabsVariants.AMBASSADOR_PAGE} />
+        <TabsMenu
+          value={value}
+          onChange={handleChange}
+          variant={TabsVariants.AMBASSADOR_PAGE}
+        />
         <IconButton startIcon={<Edit />} />
       </Box>
-      <Box component="div" style={{ width: '100%', height: '100%' }} />
+      {UserData}
     </Box>
   );
 };
