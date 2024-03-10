e65a811071c79d110b6e588f4e4c8eb2987814c5
Andrey Simonov <sasmeo@gmail.com>
2024-03-10 11:06:12 +0500

    Add left blocks

--- src/components/UserContent/LoyaltyProgram/LoyaltyProgram.tsx
+++ src/components/UserContent/LoyaltyProgram/LoyaltyProgram.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 
 const LoyaltyProgram = () => {
   return (
-    <Box style={{width: '100%', height: '100%'}} component="div">
+    <Box style={{ width: '100%', height: '100%' }} component="div">
       Программа лояльности
     </Box>
   );
--- src/pages/Ambassador/Ambassador.tsx
+++ src/pages/Ambassador/Ambassador.tsx
@@ -5,6 +5,8 @@ import { useEffect } from 'react';
 import styles from './Ambassador.module.scss';
 import { pushAmbassador } from '@/services/redux/slices/ambassadors/ambassadors';
 import { useAppDispatch } from '@/services/typeHooks';
+import AmbassadorProfile from '@/components/AmbassadorProfile/AmbassadorProfile';
+import CommentCard from '@/components/CommentCard/CommentCard';
 
 const Ambassador = () => {
   const { id } = useParams();
@@ -20,20 +22,8 @@ const Ambassador = () => {
     <>
       <div style={{ gridArea: 'controls', height: '76px' }} />
       <Content className={styles.ambassadorPage}>
-        <div
-          style={{
-            gridArea: 'profile',
-            backgroundColor: 'white',
-            height: '272px',
-          }}
-        />
-        <div
-          style={{
-            gridArea: 'comment',
-            backgroundColor: 'white',
-            height: '332px',
-          }}
-        />
+        <AmbassadorProfile />
+        <CommentCard />
         <UserContent />
       </Content>
     </>
